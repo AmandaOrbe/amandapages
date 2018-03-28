ce04c4d29f29c91cbcbe852796266ee118d425e2
(Amanda Orbe) <garayomiriam@gmail.com>
2018-03-28 19:10:42 +0200

    gallery

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,45 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+
+// var galleryImage = $(".gallery").find("img").first();
+
+
+//     var images = [
+//     "images/image1.jpg",
+//     "images/image2.jpg",
+//     "images/image3.jpg",
+//     ];
+
+// var i = 0
+
+//     setInterval(function(){
+//       i = (i + 1) % images.length;
+//       galleryImage.fadeOut(function(){
+//         $(this).attr("src", images[i]);
+//         $(this).fadeIn();
+//       });
+$(function() {
+  var galleryImage = $(".slick").find("img").first();
+  var images = $(".gallery").find("img");
+  console.log($(".numerito"));
+
+
+  var i = 0
+
+
+  $('.slick').on('click', function(){
+      i = (i + 1) % images.length;
+      galleryImage.fadeOut(function(){
+        var lula = $(".image-show").attr("src");
+        console.log(lula);
+        $(".image-show").attr("src", images[i].src);
+        $(".image-show").fadeIn();
+        $(".legend").find("span").text(i+1);
+        console.log(i+1);
+      });
+      console.log(i);
+    });
+
+});
